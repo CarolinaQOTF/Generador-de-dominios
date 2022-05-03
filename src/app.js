5a62090edced41b128e977cddd022d71becbb3e6
CarolinaQOTF <carolinacelis012@gmail.com>
2022-05-03 13:57:22 +0000

    generador de dominios subido

--- src/app.js
+++ src/app.js
@@ -7,5 +7,78 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function generate() {
+    let pronoun = ["the", "our", "your"];
+    let adj = [
+      "great",
+      "big",
+      "happy",
+      "little",
+      "Fuck",
+      "shit",
+      "brave",
+      "funny",
+      "brave",
+      "dirty",
+      "cut",
+      "tiny",
+      "hollow",
+      "square",
+      "famous",
+      "loud"
+    ];
+    let noun = [
+      "jogger",
+      "racoon",
+      "cat",
+      "dog",
+      "beer",
+      "style",
+      "sky",
+      "window",
+      "heaven",
+      "hell",
+      "bird",
+      "bacon",
+      "ball",
+      "hand",
+      "spike",
+      "cup",
+      "cloud",
+      "lips",
+      "sexy",
+      "head",
+      "little devil"
+    ];
+    let final = [
+      ".com",
+      ".io",
+      ".net",
+      ".es",
+      ".ve",
+      ".ru",
+      ".org",
+      ".tech",
+      ".onLine",
+      ".blog"
+    ];
+
+    let dominio = [];
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++)
+        for (let k = 0; k < noun.length; k++)
+          for (let l = 0; l < final.length; l++) {
+            dominio.push(pronoun[i] + adj[j] + noun[k] + final[l]);
+            console.log(pronoun[i] + adj[j] + noun[k] + final[l]);
+          }
+    }
+
+    return dominio;
+  }
+  document.querySelector("#generador1").addEventListener("click", () => {
+    var dominio2 = generate();
+    let indexRandom = Math.floor(Math.random() * dominio2.length);
+    document.querySelector("#dominio1").value = dominio2[indexRandom];
+  });
 };
